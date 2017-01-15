935bdcfc86bc56ecd3744c240fd454e7c0f42f5e
Wilmer Guerra <wilmer@targetcw.com>
2017-01-15 12:23:24 -0800

    added style to weather component

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -29,13 +29,13 @@ let Weather = React.createClass({
         var {isLoading, location, temp} = this.state;
         function renderMessage () {
             if (isLoading)
-                return <h3>Fetching Weather...</h3>;
+                return <h3 className='text-center'>Fetching Weather...</h3>;
             else if (temp && location)
                 return <WeatherMessage location={location} temp={temp} />;
         }
         return (
             <div>
-                <h3>Weather Component</h3>
+                <h1 className='text-center'>Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch} />
                 {renderMessage()}
             </div>
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ let React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
     return (
-        <h3>It is {temp} degrees in {location}</h3>
+        <h3 className='text-center'>It is {temp} degrees in {location}</h3>
     );
 };
 

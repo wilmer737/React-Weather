55c7fc3829f9f4d70e921f6aab69db36ca56077f
Wilmer Guerra <wilmer@targetcw.com>
2017-01-14 22:09:47 -0800

    Added links to examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,15 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to the examples page</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few sample example locations to try out:</p>
+            <ol>
+                <li><Link to='/?location=Oceanside'>Oceanside, CA</Link></li>
+                <li><Link to='/?location=Rio'>Rio, Brazil</Link></li>
+            </ol>
         </div>
     );
 };
